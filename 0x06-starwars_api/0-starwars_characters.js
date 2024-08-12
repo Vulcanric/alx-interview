89cf57781968b98df5d53e86972418131c925a87
Vulcanric <jneric49@gmail.com>
2024-08-12 11:36:09 +0100

    Partially done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,15 +1,48 @@
 #!/usr/bin/node
-
+/**
+ * Retreives data from an api and print it out to the console
+ *
+ * API:
+ * ----
+ *   https://swapi-api.alx-tools.com/api
+ * Description:
+ * ------------
+ *   The swapi api is the world's first quantified and programmati-
+ * cally-accessible data source for all data from the Star Wars canon universe
+ *
+ * Goal:
+ * -----
+ *   Retrieve all characters of a film given the film id as cmdline argument
+ */
 const request = require('request');
-// const JSON = require('JSON')
 
-const url = 'https://swapi-api.alx-tools.com/api/people';
-request(url, (error, respoinse, body) => {
-  console.log(typeof body);
-//	characters = JSON.parse(body.results)
-//	console.log(type(characters))
-//	for (const person of characters) {
-//		console.log(person.name)
-//	}
-}
-);
+let API = 'https://swapi-api.alx-tools.com/api/';
+
+// Get film ID
+const filmID = process.argv[2];
+API += `films/${filmID}`;
+
+request(API, (error, response, body) => {
+  try {
+    // Convert JSON string to JS Objects
+    if (error) {
+      console.error(error);
+    } else {
+      const film = JSON.parse(body);
+      const characters = film.characters;
+      // console.log(film)
+      for (const actorURL of characters) {
+        request(actorURL, (error, response, body) => {
+          if (error) {
+            console.error(error);
+          } else {
+            const actor = JSON.parse(body);
+            console.log(actor.name);
+          }
+        });
+      }
+    }
+  } catch (err) {
+    // console.error(err.message);
+  }
+});
