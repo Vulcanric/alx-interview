ce1603523c70a94d3820b2bc3ba6a2d695255da3
Vulcanric <jneric49@gmail.com>
2024-08-31 01:07:33 +0100

    Algorithm to calculate an island perimeter described in a grid context

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -19,16 +19,27 @@ def island_perimeter(grid):
     # Perimeter of island = 2 (l + b), Where l and b are the length
     # and breadth of the island respectively
     length, breadth = (0, 0)
+    n_rows, n_cols = (0, 0)
 
     for row in grid:
         landcells = 0  # Variable to count number of land cells in one row
         for cell in row:
             if cell == 1:  # Land
+                # Detecting outofbound cells (island is not really an island)
+                if n_rows == 0 or \
+                        n_rows == len(grid) - 1 or \
+                        n_cols == 0 or \
+                        n_cols == len(row) - 1:
+                    return  # Stop!
+
                 if landcells == 0:  # cell is one unit of island's length
                     length += cell
                 landcells += 1  # Count the number of land cells in a row
+            n_cols += 1
+
         if landcells > breadth:
             breadth = landcells  # Let breadth be the no. of land cells in row
+        n_rows += 1
 
     # Now calculating the perimeter of island using formula
     return 2 * (length + breadth)
